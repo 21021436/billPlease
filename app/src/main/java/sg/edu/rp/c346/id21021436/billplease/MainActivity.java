12d5ceb70c1eb03a2b38a2fdc072ce172f524828
21021436 <21021436@myrp.edu.sg>
2022-05-07 19:39:48 +0800

    Added functionality.

--- app/src/main/java/sg/edu/rp/c346/id21021436/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21021436/billplease/MainActivity.java
@@ -3,17 +3,18 @@ package sg.edu.rp.c346.id21021436.billplease;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-
-
-class multipliers {
+//Define a class Multipliers that contains public static variables called multipliers for each of service charge, gst and discount. Default is 1.
+class Multipliers {
     public static double multiplyService = 1;
     public static double multiplyGST = 1;
     public static double multiplyDiscount = 1;
@@ -38,8 +39,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //linking field variables to UI components in layout
-
+        //Link field variables to UI components in layout
         billAmount = findViewById(R.id.amount);
         numPax = findViewById(R.id.numPax);
         service = findViewById(R.id.btnSvs);
@@ -52,13 +52,16 @@ public class MainActivity extends AppCompatActivity {
         reset = findViewById(R.id.reset);
         output = findViewById(R.id.output);
 
+        //Check Cash radio button by default
+        payMethodGroup.check(R.id.payMethodCash);
 
+        //Service charge toggle button handling
         service.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-               if (service.isChecked()) multipliers.multiplyService = 1.1;
-               else multipliers.multiplyService = 1;
+               if (service.isChecked()) Multipliers.multiplyService = 1.1;
+               else Multipliers.multiplyService = 1;
             }
         });
 
@@ -66,57 +69,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (gst.isChecked()) multipliers.multiplyGST = 1.07;
-                else multipliers.multiplyGST = 1;
+                //If GST toggle is checked (on), GST multiplier is 1.07 (7%)
+                if (gst.isChecked()) Multipliers.multiplyGST = 1.07;
+                else Multipliers.multiplyGST = 1;
             }
         });
 
-
-
-
-
+        //Calculate/Split button handling which will handle the main part of the calculation and output
         split.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String billString = billAmount.getText().toString();
-                double billDouble = Double.parseDouble(billString);
-
-                String paxString = numPax.getText().toString();
-                double paxDouble = Double.parseDouble(paxString);
-
-                String discountString = discount.getText().toString();
-                if (!discountString.equals("")){
-                    double discountDouble = Double.parseDouble(discountString);
-                    multipliers.multiplyDiscount = (100 - discountDouble)/100;
+                //String trimming may not be strictly necessary, but is a good practice
+                String billString = billAmount.getText().toString().trim();
+                //Check for no entry or zero entry in Amount EditText field. The order of the conditions is important to prevent crashing.
+                if (TextUtils.isEmpty(billString) || (Double.parseDouble(billString) == 0)){
+                    //The message will be a short toast notification. The return statement breaks out of the if-condition and returns control to the main program flow.
+                    Toast.makeText(MainActivity.this, "Bill amount cannot be empty or zero!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                else multipliers.multiplyDiscount = 1;
+                //Interpret the Bill Amount as a double variable (to allow dollars and cents)
+                double billDouble = Double.parseDouble(billString.trim());
 
-                double adjustedBill = multipliers.multiplyService*multipliers.multiplyGST*multipliers.multiplyDiscount*billDouble;
+                String paxString = numPax.getText().toString().trim();
+                if (TextUtils.isEmpty(paxString)|| (Integer.parseInt(paxString) == 0)){
+                    Toast.makeText(MainActivity.this, "Number of diners cannot be empty or zero!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //Interpret the Number of Pax (diners) as an integer
+                int paxInt = Integer.parseInt(paxString);
+
+                String discountString = discount.getText().toString().trim();
+                if (!discountString.equals("")){
+                    //Interpret the discount field string as a double (only if not empty). If left empty, assume no discount (full price)
+                    double discountDouble = Double.parseDouble(discountString);
+                    //The discount multiplier will be calculated based on the discount
+                    Multipliers.multiplyDiscount = (100 - discountDouble)/100;
+                }
+                else Multipliers.multiplyDiscount = 1;
+
+                //Calculate the adjusted bill by serially multiplying the bill by all the multipliers to take into account service charge, GST and discount
+                double adjustedBill = Multipliers.multiplyService* Multipliers.multiplyGST* Multipliers.multiplyDiscount*billDouble;
+                //Format adjusted bill string to 2 decimal places to allow for dollars and cents
                 String adjustedBillString = String.format("%.2f", adjustedBill);
 
-                double eachPay = adjustedBill/paxDouble;
+                //Amount each diner pays. Division between double and int is fine as long as output is stored as double.
+                double eachPay = adjustedBill/paxInt;
                 String eachPayString = String.format("%.2f", eachPay);
 
+                //Initialise a String variable to handle the output text
                 String payMethodString;
 
+                //Determine which payment method was selected via radio button and handle the cases
                 int checkPaymentRadioId = payMethodGroup.getCheckedRadioButtonId();
 
-                if (checkPaymentRadioId == R.id.payMethodCash) {
-                    payMethodString = " in cash.";
-                }
-                else {
+                if (checkPaymentRadioId == R.id.payMethodPN) {
                     payMethodString = " via PayNow to 912345678.";
                 }
+                else {
+                    payMethodString = " in cash.";
+                }
 
-
-
-                String outputString = "Total Bill: " + adjustedBillString + "\n" + "Each Pays: " + eachPayString + payMethodString;
-
+                //String concatenation to finalise output String and send to output
+                String outputString = "Total Bill: $" + adjustedBillString + "\n" + "Each Pays: $" + eachPayString + payMethodString;
                 output.setText(outputString);
             }
         });
 
+        //Reset button handling. Restore all fields to default values
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -125,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                 discount.getText().clear();
                 service.setChecked(false);
                 gst.setChecked(false);
-                cash.setChecked(false);
+                cash.setChecked(true);
                 payNow.setChecked(false);
                 output.setText(null);
             }
